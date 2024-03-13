cc1f75959190ea331cb49d4e91a5614cdc7c7701
RUMON <rezviahmedrumon@gmail.com>
2024-03-13 20:15:10 +0600

    add partners logo

--- JS/script.js
+++ JS/script.js
@@ -101,26 +101,43 @@ sliderWrapper.addEventListener('mouseout', startAutoSlide);
 
 
 
-// logo Slider
+// carosel
 
-const logoSlides = document.querySelector('.logo-slides');
-const logoSlideItems = document.querySelectorAll('.logo-slide');
-const totalLogoSlides = logoSlideItems.length;
-let currentLogoIndex = 0;
 
-function updateLogoSlider() {
-  logoSlides.style.transform = `translateX(${-currentLogoIndex * 100}%)`;
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
 }
 
-function nextLogoSlide() {
-  currentLogoIndex = (currentLogoIndex + 1) % totalLogoSlides;
-  updateLogoSlider();
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
 
-function prevLogoSlide() {
-  currentLogoIndex = (currentLogoIndex - 1 + totalLogoSlides) % totalLogoSlides;
-  updateLogoSlider();
-}
 
-// Optional: Auto slide change
-setInterval(nextLogoSlide, 3000); // Adjust the interval (in milliseconds) as needed
+// Initialize the Slick Carousel
+$('.client-carousel').slick({
+    slidesToShow: 3, // Number of slides to show at a time
+    slidesToScroll: 1, // Number of slides to scroll at a time
+    autoplay: true, // Enable autoplay
+    autoplaySpeed: 2000, // Autoplay speed in milliseconds
+    arrows: false, // Hide navigation arrows
+    dots: false, // Hide navigation dots
+    responsive: [
+        {
+            breakpoint: 768, // Adjust the breakpoint as needed
+            settings: {
+                slidesToShow: 1
+            }
+        }
+    ]
+});
